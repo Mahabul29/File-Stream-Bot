623cabe479d0190de180c6cf51236c93d23b3d8a
Mahabul201 <Mahabulbhai288550@gmail.com>
2025-02-12 22:18:28 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -19,15 +19,15 @@ class Var(object):
     API_HASH = str(getenv('API_HASH', '679f843b6d9485bead1b81852a9634f4'))
     BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
     PICS = (environ.get('PICS', 'https://envs.sh/jUp.jpg')).split()
-    name = str(getenv('name', 'linkstreamrobot'))
+    name = str(getenv('name', 'FileToLink09_Bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002452236143'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002435810348'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5733685945").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Mahabul201'))
@@ -43,8 +43,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'TechifyBots')) 
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://mahabul201:Mahbul288550@mahabul201.83adx.mongodb.net/?retryWrites=true&w=majority&appName=Mahabul201'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Hindi_Dub_Animes_Official')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.ᴄᴏɴᴛᴀᴄᴛ @CallOwnerBot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
