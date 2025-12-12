efea5201aa121e51f5b41fd4006662dacf473581
Mahabul201 <Mahabulbhai288550@gmail.com>
2025-12-12 23:43:53 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -23,7 +23,7 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002378418888'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002384901959'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002378418888'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "120000"))  # 20 minutes
@@ -51,6 +51,7 @@ class Var(object):
     SHORTLINK = is_enabled('SHORTLINK', False)
     SHORTLINK_URL = getenv('SHORTLINK_URL', '')
     SHORTLINK_API = getenv('SHORTLINK_API', '')
+
 
 
 
